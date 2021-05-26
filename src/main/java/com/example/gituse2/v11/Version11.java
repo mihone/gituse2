87d9df0e8bea9eea305595d52ffb5d804a6b0a27
mihone <xros1178217769@gmail.com>
2021-05-26 16:48:18 +0800

    版本1.1开发

---
+++ src/main/java/com/example/gituse2/v11/Version11.java
@@ -0,0 +1,10 @@
+package com.example.gituse2.v11;
+/**
+*  @author mihone
+*  @since 2021/5/26 16:47
+*/
+public class Version11 {
+    public static void main(String[] args) {
+        System.out.println("version 1.1........");
+    }
+}
