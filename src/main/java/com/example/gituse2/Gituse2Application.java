d5209ca6920303cfa28ba36b91812aa355a6da39
Kim <xuxinyu@tenez.cn>
2021-05-26 16:32:11 +0800

    Initial commit

---
+++ src/main/java/com/example/gituse2/Gituse2Application.java
@@ -0,0 +1,13 @@
+package com.example.gituse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gituse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gituse2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gituse2/Gituse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gituse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gituse2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
