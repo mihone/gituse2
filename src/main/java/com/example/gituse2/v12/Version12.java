57a2a44299ba7356258b2d76dec8644ca8b6ff78
mihone <xros1178217769@gmail.com>
2021-05-26 16:55:08 +0800

    版本12 跌æ迭代开发

---
+++ src/main/java/com/example/gituse2/v12/Version12.java
@@ -0,0 +1,10 @@
+package com.example.gituse2.v12;
+/**
+*  @author mihone
+*  @since 2021/5/26 16:54
+*/
+public class Version12 {
+    public static void main(String[] args) {
+        System.out.println("version 12 .......");
+    }
+}
